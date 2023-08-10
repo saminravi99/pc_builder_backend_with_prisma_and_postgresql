189542e0b01d47741879b8f3ee2a87d0bfe8a52f
Samin Israr Ravi <saminisrar1@gmail.com>
2023-08-10 17:30:24 +0600

    builder service and controller

--- src/app/modules/builders/builders.controller.ts
+++ src/app/modules/builders/builders.controller.ts
@@ -4,46 +4,40 @@ import { BuildersServices } from './builders.services'
 import catchAsync from '../../../shared/catchAsync'
 import { Request, RequestHandler, Response } from 'express'
 import httpStatus from 'http-status'
+import { paginationFields } from '../../../constants/pagination'
+import pick from '../../../shared/pick'
+import { IPaginationOptions } from '../../../interfaces/pagination'
 
 const createBuilder: RequestHandler = catchAsync(
   async (req: Request, res: Response) => {
-    const { ...BuilderData } = req.body
-    const result = await BuildersServices.createBuilder(BuilderData)
+    const { buildComponentsData, ...builderData } = req.body
+    const result = await BuildersServices.createBuilder(
+      builderData,
+      buildComponentsData,
+    )
 
     sendResponse<Builder>(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'Builder created successfully!',
-      data: result,
+      data: result.data,
     })
   },
 )
 
-const updateBuilder: RequestHandler = catchAsync(
+const getBuilders: RequestHandler = catchAsync(
   async (req: Request, res: Response) => {
-    const { id } = req.params
-    const { ...BuilderData } = req.body
-    const result = await BuildersServices.updateBuilder(id, BuilderData)
+    const paginationOptions = pick(req.query, paginationFields)
+    const result = await BuildersServices.getBuilders(
+      paginationOptions as IPaginationOptions,
+    )
 
-    sendResponse<Builder>(res, {
+    sendResponse<Builder[]>(res, {
       statusCode: httpStatus.OK,
       success: true,
-      message: 'Builder updated successfully!',
-      data: result,
-    })
-  },
-)
-
-const deleteBuilder: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {
-    const { id } = req.params
-    const result = await BuildersServices.deleteBuilder(id)
-
-    sendResponse<Builder>(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Builder deleted successfully!',
-      data: result,
+      message: 'Builders fetched successfully!',
+      data: result.data,
+      meta: result.meta,
     })
   },
 )
@@ -57,20 +51,40 @@ const getBuilder: RequestHandler = catchAsync(
       statusCode: httpStatus.OK,
       success: true,
       message: 'Builder fetched successfully!',
-      data: result,
+      data: result.data,
     })
   },
 )
 
-const getBuilders: RequestHandler = catchAsync(
+const updateBuilder: RequestHandler = catchAsync(
   async (req: Request, res: Response) => {
-    const result = await BuildersServices.getBuilders()
+    const { id } = req.params
+    const { buildComponentsData, ...builderData } = req.body
+    const result = await BuildersServices.updateBuilder(
+      id,
+      builderData,
+      buildComponentsData,
+    )
 
-    sendResponse<Builder[]>(res, {
+    sendResponse<Builder>(res, {
       statusCode: httpStatus.OK,
       success: true,
-      message: 'Builders fetched successfully!',
-      data: result,
+      message: 'Builder updated successfully!',
+      data: result.data,
+    })
+  },
+)
+
+const deleteBuilder: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params
+    const result = await BuildersServices.deleteBuilder(id)
+
+    sendResponse<Builder>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Builder deleted successfully!',
+      data: result.data,
     })
   },
 )
--- src/app/modules/builders/builders.services.ts
+++ src/app/modules/builders/builders.services.ts
@@ -1,49 +1,171 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { PrismaClient, Builder } from '@prisma/client'
+import { PrismaClient, Builder, BuildComponents } from '@prisma/client'
+import { IGenericResponse } from '../../../interfaces/common'
+import { IPaginationOptions } from '../../../interfaces/pagination'
+import { paginationHelpers } from '../../../helpers/paginationHelper'
 
 const prisma = new PrismaClient()
 
-const createBuilder = async (data: Builder): Promise<Builder> => {
-  const newBuilder = await prisma.builder.create({
-    data,
+const createBuilder = async (
+  builderData: Builder,
+  buildComponentsData: BuildComponents[],
+): Promise<IGenericResponse<Builder>> => {
+  const builderWithComponents = await prisma.$transaction(async prisma => {
+    const builder = await prisma.builder.create({
+      data: {
+        ...builderData,
+        buildComponents: {
+          create: buildComponentsData,
+        },
+      },
+      include: {
+        buildComponents: {
+          include: {
+            components: {
+              include: {
+                category: true,
+              },
+            },
+          },
+        },
+      },
+    })
+
+    return builder
   })
 
-  return newBuilder
+  return {
+    data: builderWithComponents,
+  }
 }
 
-const updateBuilder = (
+const getBuilders = async (
+  paginationOptions: IPaginationOptions,
+): Promise<IGenericResponse<Builder[]>> => {
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions)
+  const builders = await prisma.builder.findMany({
+    include: {
+      buildComponents: {
+        include: {
+          components: {
+            include: {
+              category: true,
+            },
+          },
+        },
+      },
+    },
+    take: limit,
+    skip,
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
+  })
+  const total = await prisma.builder.count()
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: builders,
+  }
+}
+
+const getBuilder = async (
   id: number | string,
-  data: Builder,
-): Promise<Builder | null> => {
-  const updatedBuilder = prisma.builder.update({
+): Promise<IGenericResponse<Builder | null>> => {
+  const Builder = await prisma.builder.findUnique({
     where: { id: Number(id) },
-    data,
+    include: {
+      buildComponents: {
+        include: {
+          components: {
+            include: {
+              category: true,
+            },
+          },
+        },
+      },
+    },
   })
 
-  return updatedBuilder
+  return {
+    data: Builder,
+  }
 }
 
-const deleteBuilder = (id: number | string): Promise<Builder | null> => {
-  const deletedBuilder = prisma.builder.delete({
-    where: { id: Number(id) },
+const updateBuilder = async (
+  id: number | string,
+  builderData: Builder,
+  buildComponentsData: BuildComponents[],
+): Promise<IGenericResponse<Builder | null>> => {
+  const updatedBuilder = await prisma.$transaction(async prisma => {
+    const builder = await prisma.builder.update({
+      where: { id: Number(id) },
+      data: {
+        ...builderData,
+        buildComponents: {
+          deleteMany: {},
+          create: buildComponentsData,
+        },
+      },
+      include: {
+        buildComponents: {
+          include: {
+            components: {
+              include: {
+                category: true,
+              },
+            },
+          },
+        },
+      },
+    })
+
+    return builder
   })
 
-  return deletedBuilder
+  return {
+    data: updatedBuilder,
+  }
 }
 
-const getBuilder = (id: number | string): Promise<Builder | null> => {
-  const Builder = prisma.builder.findUnique({
-    where: { id: Number(id) },
+const deleteBuilder = async (
+  id: number | string,
+): Promise<IGenericResponse<Builder | null>> => {
+  const deletedBuilder = await prisma.$transaction(async prisma => {
+    // Find the builder's associated build component IDs
+    const builder = await prisma.builder.findUnique({
+      where: { id: Number(id) },
+      include: {
+        buildComponents: true,
+      },
+    })
+
+    // Delete associated build components
+    if (builder) {
+      const buildComponentIds = builder.buildComponents.map(
+        component => component.id,
+      )
+      await prisma.buildComponents.deleteMany({
+        where: { id: { in: buildComponentIds } },
+      })
+    }
+
+    // Delete the builder after its associated build components are deleted
+    await prisma.builder.delete({
+      where: { id: Number(id) },
+    })
+
+    return builder
   })
 
-  return Builder
-}
-
-const getBuilders = (): Promise<Builder[]> => {
-  const Builders = prisma.builder.findMany()
-
-  return Builders
+  return {
+    data: deletedBuilder,
+  }
 }
 
 export const BuildersServices = {
